3d4e7de44a1ab620892ce03b997721ee5a959932
Tin Dang <tin2dang@gmail.com>
2023-10-26 16:59:57 -0500

    added searching by single job

--- src/main/java/dev/tindang/resume/JobController.java
+++ src/main/java/dev/tindang/resume/JobController.java
@@ -1,13 +1,16 @@
 package dev.tindang.resume;
 
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/jobs")
@@ -17,5 +20,10 @@ public class JobController {
     @GetMapping
     public ResponseEntity<List<Job>> getAllJobs() {
         return new ResponseEntity<List<Job>>(jobService.allJobs(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Optional<Job>> getSingleJob(@PathVariable ObjectId id) {
+        return new ResponseEntity<Optional<Job>>(jobService.singleJob(id), HttpStatus.OK);
     }
 }
--- src/main/java/dev/tindang/resume/JobService.java
+++ src/main/java/dev/tindang/resume/JobService.java
@@ -1,9 +1,11 @@
 package dev.tindang.resume;
 
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class JobService {
@@ -12,5 +14,8 @@ public class JobService {
     private JobRepositiory jobRepositiory;
     public List<Job> allJobs(){
         return jobRepositiory.findAll();
+    }
+    public Optional<Job> singleJob(ObjectId id) {
+        return jobRepositiory.findById(id);
     }
 }
